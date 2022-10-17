cb39fc57ddd9fb09be3f333d979849e3f33a08f3
Tolegen Timur <demigodkz@gmail.com>
2022-10-17 20:50:23 +0600

    feat: add Courses component for mobile devices

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -4,18 +4,33 @@ import { withLayout } from '../../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../../interfaces/menu.interface';
 import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
-import { ParsedUrlQuery } from 'node:querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
+import Head from 'next/head';
+import { Error404 } from '../404';
+import { ParsedUrlQuery } from 'querystring';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-	return <TopPageComponent
-		firstCategory={firstCategory}
-		page={page}
-		products={products}
-	/>;
+	if (!page || !products) {
+		return <Error404 />;
+	}
+
+	return <>
+		<Head>
+			<title>{page.metaTitle}</title>
+			<meta name="description" content={page.metaDescription} />
+			<meta property="og:title" content={page.metaTitle} />
+			<meta property="og:description" content={page.metaDescription} />
+			<meta property="og:type" content="article" />
+		</Head>
+		<TopPageComponent
+			firstCategory={firstCategory}
+			page={page}
+			products={products}
+		/>
+	</>;
 }
 
 export default withLayout(TopPage);
